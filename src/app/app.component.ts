c1d1faf21c53181bdead915c3bdbf24d511a4937
noam1610 <nbenlolo@yoobic.com>
2018-01-29 10:42:59 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,95 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { Map } from 'mapbox-gl';
-import { environment } from '../environments/environment';
-import * as jsonfile from 'jsonfile'
+import {
+    Map
+} from 'mapbox-gl';
+import {
+    environment
+} from '../environments/environment';
+import {
+    Http,
+    Response,
+    Headers,
+    RequestOptions
+} from '@angular/http';
+import * as request from 'request';
+
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
-
-
-export class AppComponent   implements OnInit{
+export class AppComponent implements OnInit {
     title = 'app';
+    data_polyg;
 
-    ngOnInit() {
-    
-        mapboxgl.accessToken = 'pk.eyJ1Ijoibm9hbTE2MTAiLCJhIjoiY2phamgzdzFxMjlhbTMzbGV1aWg4MWY3dyJ9.ozjyeDjaIBuTLIlMsYeMbg';
-            var map = new mapboxgl.Map({
-                container: 'map',
-                style: 'mapbox://styles/noam1610/cjcyy7a72041t2squlngaxgdw',
-                center: [-87.8, 41.87],
-                zoom: 10
-        });
 
-        jsonfile.readFile("chicago.geojson", function(err, obj) {
-            console.dir(obj)
-        })
+    constructor() {
+
     }
 
+    ngOnInit() {
 
- 
+        var list_district = [];
+        let getRandomColor = function() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        };
+
+        mapboxgl.accessToken = 'pk.eyJ1Ijoibm9hbTE2MTAiLCJhIjoiY2phamgzdzFxMjlhbTMzbGV1aWg4MWY3dyJ9.ozjyeDjaIBuTLIlMsYeMbg';
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/noam1610/cjcyy7a72041t2squlngaxgdw',
+            center: [-87.8, 41.87],
+            zoom: 10
+        });
+
+
+        map.on('load', function() {
+
+            request("https://raw.githubusercontent.com/Bended/bsafe/master/noam.json", function(error, response, body) {
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+                this.data_polyg = JSON.parse(body).features;
+
+                for (var i = 0; i < this.data_polyg.length; i++) {
+                    var elem = this.data_polyg[i];
+                    if (!list_district.includes(elem.properties.beat_num)) {
+                        list_district.push(elem.properties.beat_num)
+                        console.log(elem.properties.beat_num)
+                        map.addLayer({
+                            'id': elem.properties.beat_num,
+                            'type': 'fill',
+                            'source': {
+                                'type': 'geojson',
+                                'data': {
+                                    'type': 'Feature',
+                                    'geometry': elem.geometry
+                                }
+                            },
+                            'layout': {},
+                            'paint': {
+                                'fill-color': getRandomColor(),
+                                'fill-opacity': 0.6
+                            }
+                        });
+
+                    }
+                }
+            });
+            console.log(this.data_polyg);
+
+        })
+    }
 }
